712158a61ae5b9882abb0678f6af0ba8075dc0e4
Worth Baker <worthbak@gmail.com>
2021-11-08 15:21:41 -0700

    Initial Commit

---
+++ InventoryWatch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InventoryWatch
+//
+//  Created by Worth Baker on 11/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InventoryWatch/InventoryWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryWatchApp.swift
+//  InventoryWatch
+//
+//  Created by Worth Baker on 11/8/21.
+//
+
+import SwiftUI
+
+@main
+struct InventoryWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
